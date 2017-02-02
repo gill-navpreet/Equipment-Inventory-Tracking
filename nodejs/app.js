de44132ce7ac3e70706699b2e7becb9d1aa406b6
iahays <iahays@ucdavis.edu>
2017-02-01 17:06:38 -0800

    database queries based on url entry.  removed select.js and mailer.js for they no longer have a use.

--- nodejs/app.js
+++ nodejs/app.js
@@ -11,34 +11,109 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-var data = {
+
+
+var seanData = {
     id: null,
     name: 'Sean',
     qrlcode: 9382792
 };
 
-var query = connection.query('insert into inventory set ?', data, function(err, result) {
+var westonData = {
+    id: null,
+    name: 'Weston',
+    qrlcode: 3729938
+}
+
+var selectQuery = function() {connection.query('select * from inventory', function (err,result) {
+    if(err){
+        console.error(err);
+        return;
+    }
+    console.log(result);
+});}
+
+
+var seanQuery = function() {connection.query('insert into inventory set ?', seanData, function(err, result) {
   if(err){
       console.error(err);
       return;
   }
   console.error(result);
-});
+});}
 
-function send404Response(response){
-    response.writeHead(404, {"Content-Type": "text/plain"});
-    response.write("Error 404: Page not found!");
-    response.end();
+var westonQuery = function() {connection.query('insert into inventory set ?', westonData, function(err, result) {
+    if(err){
+        console.error(err);
+        return;
+    }
+    console.error(result);
+});}
+
+var deleteQuery = function() {
+    connection.query('delete from inventory where name = "Sean"', function (err,result) {
+        console.log(result);
+    });
+    connection.query('delete from inventory where name = "Weston"', function (err,result) {
+        console.log(result);
+    });
 }
 
 function onRequest(request,response){
     if( request.method == 'GET' && request.url == '/'){
         response.writeHead(200,{"Content-Type": "text/html"});
         fs.createReadStream("./index.html").pipe(response);
+        console.log("Connected to Home");
     }else{
+
+    if(request.method == 'GET' && request.url == '/insertWeston'){
+        westonRequest(request,response);
+    }else{
+
+    if(request.method == 'GET' && request.url == '/insertSean'){
+        seanRequest(request,response);
+    }else {
+    if (request.method == 'GET' && request.url == '/deleteEntries') {
+        deleteRequest(request, response);
+    }else {
         send404Response(response);
     }
+    }
+    }
+    }
+
 }
+function westonRequest(request,response){
+    response.writeHead(200,{"Content-Type": "text/html"});
+    fs.createReadStream("./insertWeston.html").pipe(response);
+    console.log("Connected to westonRequest");
+    westonQuery();
+    selectQuery();
+}
+
+function seanRequest(request,response){
+    response.writeHead(200,{"Content-Type": "text/html"});
+    fs.createReadStream("./insertSean.html").pipe(response);
+    console.log("Connected to seanRequest");
+    seanQuery();
+    selectQuery();
+}
+
+function deleteRequest(request,response){
+    response.writeHead(200,{"Content-Type": "text/html"});
+    fs.createReadStream("./deleteEntries.html").pipe(response);
+    console.log("Connected to deleteRequest");
+    deleteQuery();
+    selectQuery();
+}
+
+//error response
+function send404Response(response){
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("Error 404: Page not found!");
+    response.end();
+}
+
 
 http.createServer(onRequest).listen(8000);
 console.log("Server is now running....");
--- nodejs/mailer.js
+++
@@ -1,3 +0,0 @@
-// nodemailer implementation with express
-// https://www.youtube.com/watch?v=3Z-YnINIJQ8
-var nodemailer = require('nodemailer');
