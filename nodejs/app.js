64450674d3deac597361db04a644e3c53814255c
iahays <iahays@ucdavis.edu>
2017-02-22 09:04:14 -0800

    Updated project status in comments. Introduced delete button. Deletes all entries (for testing purposes only). Page now updates when submit buttons clicked.

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,7 +3,8 @@
  *
  * status of project:
  * This project currently has the ability to connect to database (MySQL) server that communicates with nodejs (backend) server.
- * Has functions that can query database
+ * Has functions that can query database through submit buttons. Currently there are only insert and delete queries. Buttons now
+ * update page when pressed and no refreshing required.
  *
  * We have the homepage set to home.html which provides us a form to fill out user information. This information is linked to a
  * submit button which is implemented to send data to our backend through to the database.
@@ -11,16 +12,18 @@
  * Our Angular (frontend) has the capability to retrieve information from the nodejs server by importing a json file that is sent
  * out through the bodyparser module. The information from the database can be seen under the form when you load the page.
  *
- * when submitting information in the form on the webpage home.html, you must refresh the page to see the update.
+ * when submitting information in the form on the webpage home.html, it actively updates the entries shown on the page
+ *
+ * when deleting, it deletes ALL entries in the database. This is for testing purposes only. We need to implement a future
+ * delete button that will delete based on specified information.
+ *
+ * There was a temporary deleteAll query added to the program to implement this test.
  *
  * TODO:
- * Set up more buttons that can do the options that relate to the database for nodejs to handle. These buttons
- * may include delete queries,
- *
- * We want the entries to be updated through run time through the use of ???AJAX??? not sure how to do this, but will have to
- * research, or some other implementation.
- *
  * We can experiment with bootstrap on the forms for now
+ *
+ * create a plan for buttons that can manipulate the database using queries. These buttons can delete based on specified
+ * information, Update user information.
  *
  * We want to separate home.html into two files. There exists a home.js, but it isn't up to date with home.html. I've
  * struggled to separate the home.html file into a .js for some reason. This should be a simple fix, but is now a priority
@@ -33,23 +36,22 @@ var express = require ('express');
 var path = require('path');
 //mysql module, allows for database access.
 var mysql = require ('mysql');
-//router
+//router (not yet used)
 var router = express.Router();
 //body parser
 var bodyParser = require('body-parser');
 
 
 //good practice when setting up port.
-var PORT = process.env.PORT || 3006;
+var PORT = process.env.PORT || 3005;
 
 //mysql connection through mysql workbench server
 var connection = mysql.createConnection({
-    host: '' ,
-    user: '',
-    password: '',
-    database: ''
+    host: 'ecs193instancedb.chg90xdsb0jq.us-west-1.rds.amazonaws.com',
+    user: 'ecs193db',
+    password: 'Commentecs193',
+    database: 'Inventory_Tracking_193'
 });
-
 //connects to mysql server
 connection.connect(function(err){
     if(err){
@@ -87,11 +89,10 @@ var selectQuery = function(table) {
             return;
         }
         console.log('Data received from Db:\n');
-        console.log(rows);
         jsonQuery = rows; //the jsonQuery variable from above.
     })
 };
-//mysql delete query function. Deletes from a table by id
+//mysql delete query function. Deletes from a table by id (not yet used)
 var deleteQuery = function(table,id) {
     connection.query('DELETE FROM ' + table + ' WHERE id = ?', id, function (err, result) {
         if (err) {
@@ -101,7 +102,19 @@ var deleteQuery = function(table,id) {
         console.log('Deleted ' + result.affectedRows + ' rows');
     })
 };
-//mysql update query function.
+
+
+//mysql delete all query function. Dangerous function, deletes all entries. This is only here for testing purposes.
+var deleteAllQuery = function(table) {
+    connection.query('DELETE FROM ' + table, function (err, result) {
+        if (err) {
+            console.log('Error in deleteQuery');
+            return;
+        }
+        console.log('Deleted ' + result.affectedRows + ' rows');
+    })
+};
+//mysql update query function. (not yet used)
 var updateQuery = function(table,dataType,dataName,id) {
     connection.query('UPDATE ' + table + ' SET ' + dataType + ' = ? Where ID = ?',[dataName,id], function(err, result){
         if(err) {
@@ -119,11 +132,18 @@ var updateQuery = function(table,dataType,dataName,id) {
 
 
 //this inserts into database from angular front end
-app.post('/api/entry', function(req,res,next){
+app.post('/insert', function(req,res,next){
     var entry = req.body;
     insertQuery('Persons',entry);
+
+
 });
 
+//this deletes every entry in the database. Dangerous use, needs to specify what to delete for future.
+app.post('/delete', function(req,res,next){
+    var entry = req.body;
+    deleteAllQuery('Persons',entry);
+});
 
 //home, access by localhost:3003
 //home is set up to enter in a data entry, but does not record it yet.
