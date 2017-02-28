a5ec6fb7ab8826f1f70f8d5516d582e5393aa717
iahays <iahays@ucdavis.edu>
2017-02-28 13:49:56 -0800

    Barcode Generator implemented. Fixed problem where scanner would not bind to the input field for angular to recognize. Form submits perfect now

--- app.js
+++ app.js
@@ -58,10 +58,10 @@ var PORT = process.env.PORT || 3000;
 
 //mysql connection through mysql workbench server
 var connection = mysql.createConnection({
-    host: 'ecs193instancedb.chg90xdsb0jq.us-west-1.rds.amazonaws.com',
-    user: 'ecs193db',
-    password: 'Commentecs193',
-    database: 'Inventory_Tracking_193'
+    host: '',
+    user: '',
+    password: '',
+    database: ''
 });
 //connects to mysql server
 connection.connect(function(err){
